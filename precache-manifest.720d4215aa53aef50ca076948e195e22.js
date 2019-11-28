7a12ef146c713318c2772067b33eb54e6739a497
Woody <kazettique@gmail.com>
2019-11-28 16:24:11 +0800

    Updates

---
+++ precache-manifest.720d4215aa53aef50ca076948e195e22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe1b1efcd8bdf6f1c9e44c32e1c1a5d",
+    "url": "/react-charts-app/index.html"
+  },
+  {
+    "revision": "d82d308b30cb46118937",
+    "url": "/react-charts-app/static/css/main.718a15d6.chunk.css"
+  },
+  {
+    "revision": "fae5f27266cf0115c3a1",
+    "url": "/react-charts-app/static/js/2.cb6b38a0.chunk.js"
+  },
+  {
+    "revision": "d82d308b30cb46118937",
+    "url": "/react-charts-app/static/js/main.94ac45d6.chunk.js"
+  },
+  {
+    "revision": "8b4d88f2b0db40904474",
+    "url": "/react-charts-app/static/js/runtime-main.0f936282.js"
+  }
+]);
